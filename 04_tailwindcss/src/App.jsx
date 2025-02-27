b0aac7d5737ec2e1401f4d763f7ed331c9b6a1f3
Himanshu-Khairnar <himanshuk1205@gmail.com>
2025-02-27 10:36:29 +0530

    Completed props and started tailwindcss

---
+++ 04_tailwindcss/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './components/Card'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="font-4xl">Heello</h1>
+      <Card cardName="Monkey" />
+      <Card cardName="Man" />
+    </div>
+  );
+}
